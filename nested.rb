998f325fc2a21be5b636b8a47f775d8719671ff0
Angus Morrison <contact@angus-morrison.com>
2019-08-07 13:31:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,5 @@ def adding_to_dennis
         }
      }
 
-  return programmer_hash[:dennis_ritchie][:language] << "Assembly"
+  return programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
